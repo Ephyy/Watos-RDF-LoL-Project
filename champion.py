fe41b63e3a173a40d17b2b90d101af3740a6f972
Tobal <cristobal.jaramillo@ug.uchile.cl>
2021-11-27 21:00:58 -0300

    adding rdf for champion class

--- champion.py
+++ champion.py
@@ -1,12 +1,19 @@
 class Champion:
+    
+    """
+    Class Champion
+    
+    """
 
-    def __init__(self, name, health, damage,
+    def __init__(self, name, attack, defense, magic, difficulty,
                  primary_position, info, key, partype,
                  playstyle, region, relation, tags,
-                 title, version):
+                version):
         self.name = name
-        self.health = health
-        self.damage = damage
+        self.attack = attack
+        self.defense = defense
+        self.magic = magic
+        self.difficulty = difficulty
         self.primary_position = primary_position
         self.info = info
         self.key = key
@@ -15,15 +22,51 @@ class Champion:
         self.region = region
         self.relation = relation
         self.tags = tags
-        self.title = title
         self.version = version
+        
+    
+    def rdfs_class_type(self):
+        return f'ex:{self.name} ex:type ex:Champion'
+    
+    def rdfs_primary_position(self):
+        return f'ex:primaryPosition ex:{self.primary_position}'
+    
+    def rdfs_region(self):
+        return f'ex:region ex:{self.region}'
+    
+    
+    
+    def rdf_champ(self):
+        
+        """
+        Create RDF for a Champion
+        
+        """
+        
+        print(f'''{self.rdfs_class_type()}
+              \t {self.rdfs_primary_position()}
+              \t {self.rdfs_region()}
+              '''
+              
+              )
         
 
 def create_champion(dict_info):
     
+    """
+    create a champion object from a dictionary
+    
+    :param dict_info: dictionary with the champion information
+    
+    :return champion: champion object
+    
+    """
+    
     name = dict_info['name']
-    health = dict_info['stats']['hp']
-    damage = dict_info['stats']['attackdamage']
+    attack = dict_info['info']['attack']
+    defense = dict_info['info']['defense']
+    magic = dict_info['info']['magic']
+    difficulty = dict_info['info']['difficulty']
     primary_position = dict_info["PrimaryPosition"]
     info = dict_info['info']
     key = dict_info['key']
@@ -32,17 +75,26 @@ def create_champion(dict_info):
     region = dict_info['region']
     relation = dict_info['relation']
     tags = dict_info['tags']
-    title = dict_info['title']
     version = dict_info['version']
     
-    return Champion(name, health, damage,
+    return Champion(name, attack, defense, magic, difficulty,
                     primary_position, info, key, partype,
                     playstyle, region, relation, tags,
-                    title, version)
+                    version)
 
 def json_to_champion(json_champ):
     
+    """
+    create a list of champion objects from a json file
+    
+    :param json_champ: json file with the champions information
+    
+    :return champions: list of champion objects
+    
+    """
+    
     list_champ = []
     for champ in json_champ:
-        print(champ)
         list_champ.append(create_champion(json_champ[champ]))
+        
+    return list_champ
